f396d6a6d7b9ee36bcd601c4c5f10af4ba149a53
Yoann Rouquie <rouquieyoann@gmail.com>
2024-10-28 16:28:46 +0100

    chore: wip on Dashboard Component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    // lazy loading
+    loadComponent: () =>
+      import('./features/dashboard/dashboard.component').then(
+        (component) => component.DashboardComponent
+      ),
+  },
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+];
