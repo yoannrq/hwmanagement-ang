8ada0732e869bb67b0910995f8ddb0e1c42af2fa
Yoann Rouquié <120712901+yoannrq@users.noreply.github.com>
2024-10-29 13:37:18 +0100

    feat: Add http interceptor +  Get users count from database in dashboard component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-// app.component.ts
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
 
 @Component({
--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Observable, map, catchError, of } from 'rxjs';
+
+import { UserService } from '../../core/services/user.service';
+import { User } from '../../core/models/user.model';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,6 +12,21 @@ import { CommonModule } from '@angular/common';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent {
-  constructor() {}
+export class DashboardComponent implements OnInit {
+  userCount$: Observable<number>;
+
+  constructor(private userService: UserService) {
+    this.userCount$ = new Observable();
+  }
+
+  ngOnInit(): void {
+    // Init userCount$ with the number of users
+    this.userCount$ = this.userService.getUsers().pipe(
+      map((users: User[]) => users.length),
+      catchError((error) => {
+        console.error('Erreur lors du chargement des utilisateurs:', error);
+        return of(0);
+      })
+    );
+  }
 }
